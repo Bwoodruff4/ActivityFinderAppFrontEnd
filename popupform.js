ca60ada28f2815f0a785dfd0e1b7152803a617cc
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-05-07 15:55:26 -0600

    Added detail to user show page

--- popupform.js
+++ popupform.js
@@ -4,4 +4,19 @@ function openForm() {
   
 function closeForm() {
     document.getElementById("create-user-form").style.display = "none";
+}
+
+function openFormProfile() {
+    document.getElementById("update-user-form").style.display = "flex";
+}
+  
+function closeFormProfile() {
+    document.getElementById("update-user-form").style.display = "none";
+} 
+function openFormActivity() {
+    document.getElementById("add-activity-form").style.display = "flex";
+}
+  
+function closeFormActivity() {
+    document.getElementById("add-activity-form").style.display = "none";
 } 
